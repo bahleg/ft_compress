de7efba0f95ef10d8382108bfcb7eb91c52ded92
bahleg <bakhteev@phystech.edu>
2020-01-20 23:36:09 +0300

    Shelve

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 * 0.1.0 --- basic compressors
 * 0.2.0 --- updated interfaces. Dumb compressor works correctly.
 * 0.3.0 --- Compressor8bit was refactored
+* 0.4.0 --- Shelve-based storage
---
+++ ft_compress/storages/shelve_based.py
@@ -0,0 +1,49 @@
+from ft_compress.interfaces.storage import Storage
+import shelve 
+
+class BucketEmulator:
+    def __init__(self, db, prefix):
+        self.db = db 
+        self.prefix = prefix
+    
+    def __getitem__(self, key):
+        return self.db['{0}.{1}'.format(self.prefix, key)]
+    
+    def __setitem__(self, key, value):
+        self.db['{0}.{1}'.format(self.prefix, key)] = value 
+        
+
+class ShelveStorage(Storage):
+    def __init__(self, filename, flag, **shelve_args):
+        super(Storage, self).__init__()        
+        self._db = shelve.open(filename, flag=flag, **shelve_args)
+        self.buckets = {}
+
+
+    def get_bucket(self, bucket):
+        return self.buckets.get(bucket, None)
+        
+    
+    def create_bucket(self, bucket):
+        self.buckets[bucket] = BucketEmulator(self._db, bucket)
+        return self.buckets[bucket]
+    
+    def __repr__(self):
+        return 'Shelve Storage'
+        
+
+    
+            
+if __name__=='__main__':
+    d = ShelveStorage('test', 'c')
+    print ('empty bucket:', d['test'])
+    d['test']['123'] = '456'
+    d['test2'][789] ='1023'
+    print (d['test']['123'])
+    print (d['test2'][789])
+    exc = False 
+
+    d = ShelveStorage('test','r')
+    print (d['test']['123'])
+    print (d['test2'][789])
+    print (d)
